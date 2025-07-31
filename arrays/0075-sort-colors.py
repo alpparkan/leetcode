91172215dbc4ef89c10f3687cd1a140da48b01c5
alp <alpparkan@gmail.com>
2025-08-01 01:30:36 +0200

    solutions for problem 61 and problem 75

---
+++ arrays/0075-sort-colors.py
@@ -0,0 +1,25 @@
+from typing import List
+"""
+    https://leetcode.com/problems/sort-colors
+    Difficuilty: Medium
+
+    Time complexity: O(n)
+    Space complexity: O(n)
+
+    Solution: 2 pass - using an additional array store counts of colors and re-assign original array in the second pass
+"""
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        counts = [0] * 3
+        for n in nums:
+            counts[n] += 1
+        
+        index = 0
+        for color, count in enumerate(counts):
+            for _ in range(count):
+                nums[index] = color
+                index += 1
---
+++ linked-list/0061-rotate-list.py
@@ -0,0 +1,45 @@
+from typing import Optional
+"""
+    https://leetcode.com/problems/rotate-list
+    Difficuilty: Medium
+
+    Time complexity: O(n)
+    Space complexity: O(1)
+
+    Solution: get the length of the list and calculate the rotation point, then prepend the list with rotated part.
+"""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        length = 0
+        tmp = head
+        while tmp:
+            length += 1
+            tmp = tmp.next
+        
+        if k == 0 or not head or length == 1:
+            return head
+
+        rotateNum = k % length
+        if rotateNum > 0:
+            i = length - rotateNum - 1
+            tmp = head
+            while i > 0:
+                i -= 1
+                tmp = tmp.next
+            newHead = tmp.next
+            tmp.next = None
+
+            tmp = newHead
+            while tmp.next:
+                tmp = tmp.next
+            tmp.next = head
+            return newHead
+        return head
